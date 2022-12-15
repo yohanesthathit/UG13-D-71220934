e012b505218e2314548ce85f273fb916b0cabee4
yohanesthathit <117900755+yohanesthathit@users.noreply.github.com>
2022-12-15 16:26:54 +0700

    Add files via upload

---
+++ UG13_D_71220934.py
@@ -0,0 +1,36 @@
+import turtle
+t=turtle
+t.bgcolor("slateblue")
+
+import turtle as r
+
+r.pensize(40)
+r.color("white")
+
+r.right(50)
+r.forward(100)
+
+
+r.right(230)
+r.forward(100)
+r.backward(150)
+
+import turtle as q
+
+q.penup()
+q.setpos(40,160)
+q.pendown()
+
+q.pensize(40)
+q.color("white")
+q.forward(150)
+q.backward(75)
+q.right(90)
+q.forward(150)
+
+sc = turtle.Screen().exitonclick()
+
+
+
+
+
